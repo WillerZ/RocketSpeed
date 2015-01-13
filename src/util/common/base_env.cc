5aecebe125bf3112e62f129f1be06ab7e8a5ad13
Dhruba Borthakur <dhruba@fb.com>
2015-01-13 02:16:20 -0800

    pthread_getname_np is not defined on Android.
    Summary: pthread_getname_np is not defined on Android.
    
    Test Plan: compile on android
    
    Reviewers: stupaq-fb
    
    Reviewed By: stupaq-fb
    
    Subscribers: rocketspeed
    
    Differential Revision: https://reviews.facebook.net/D31413

--- src/util/common/base_env.cc
+++ src/util/common/base_env.cc
@@ -10,11 +10,13 @@
 
 namespace {
 std::string gettname() {
+#if !defined(ROCKETSPEED_PLATFORM_ANDROID)
   char name[64];
   if (pthread_getname_np(pthread_self(), name, sizeof(name)) == 0) {
     name[sizeof(name)-1] = 0;
     return std::string(name);
   }
+#endif
   return "";
 }
 
