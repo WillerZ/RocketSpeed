d293a8544099f3b541be721601e5437b1985384d
Syed Emran <semran@fb.com>
2015-01-30 22:47:52 -0800

    MurmurHash2 for 32 bit platform
    Summary: Implement Murmurhash2 for 32 bit platform
    
    Test Plan:
    Ran 'make all check'
    Could not find this hash specific unit tests.
    
    Reviewers: stupaq-fb
    
    Reviewed By: stupaq-fb
    
    Subscribers: rocketspeed
    
    Differential Revision: https://reviews.facebook.net/D32403

--- src/util/common/hash.h
+++ src/util/common/hash.h
@@ -96,4 +96,80 @@ struct MurmurHash2<std::string> {
   }
 };
 
+
+/**
+ * 32-bit MurmurHash2 implementation.
+ * Based on:
+ * https://github.com/lemire/StronglyUniversalStringHashing/blob/master/smhasherpackage/MurmurHash2.cpp
+ */
+template <typename... T>
+struct MurmurHash2_32;
+
+template <>
+struct MurmurHash2_32<size_t> {
+  size_t operator()(size_t xa) const {
+    uint32_t x = xa;
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+    uint32_t h = 0xfacec0de;
+    x *= m;
+    x ^= x >> r;
+    x *= m;
+    h ^= x;
+    h *= m;
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+    return static_cast<size_t>(h);
+  }
+};
+
+
+template <>
+struct MurmurHash2_32<Slice> {
+  size_t operator()(Slice x) const {
+    size_t len = x.size();
+    const uint32_t seed = 0xfacec0de;
+
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+
+    // Initialize the hash to a 'random' value
+    uint32_t h = seed ^ len;
+
+    // Mix 4 bytes at a time into the hash
+    const uint32_t* data = reinterpret_cast<const uint32_t*>(x.data());
+
+    while (len >= 4) {
+      uint32_t k = *data++;
+      k *= m;
+      k ^= k >> r;
+      k *= m;
+
+      h *= m;
+      h ^= k;
+
+      len -= 4;
+    }
+
+    // Handle the last few bytes of the input array
+
+    const unsigned char* data2 = reinterpret_cast<const unsigned char*>(data);
+    switch (len) {
+      case 3: h ^= uint32_t(data2[2]) << 16;
+      case 2: h ^= uint32_t(data2[1]) << 8;
+      case 1: h ^= uint32_t(data2[0]);
+        h *= m;
+    }
+
+    // Do a few final mixes of the hash to ensure the last few
+    // bytes are well-incorporated.
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
+  }
+};
+
 }  // namespace rocketspeed
