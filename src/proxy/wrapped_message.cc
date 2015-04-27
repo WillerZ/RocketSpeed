a2a0fc2c87e5769b68d8a0eaa7163f9fc349e28d
Mateusz Machalica <stupaq@fb.com>
2015-04-27 07:42:30 -0700

    Removed magic from MQTT wrapper protocol.
    Summary: above
    
    Test Plan: `make -j all check`, there is a test for that.
    
    Reviewers: rocketspeed, pja
    
    Reviewed By: pja
    
    Subscribers: rocketspeed, dhruba
    
    Differential Revision: https://reviews.facebook.net/D37725

--- src/proxy/wrapped_message.cc
+++ src/proxy/wrapped_message.cc
@@ -15,89 +15,47 @@ namespace rocketspeed {
 
 namespace {
 
-const uint32_t kHeaderMagic = 0xD00DBEEF, kFooterMagic = 0xFEE1DEAD;
-
-const size_t kHeaderSize = sizeof(kHeaderMagic),
-             kFooterSize = sizeof(StreamID) + sizeof(MessageSequenceNumber) +
-                           sizeof(kFooterMagic);
-
-std::string MagicToHexString(const uint32_t magic) {
-  return Slice(reinterpret_cast<const char*>(&magic), sizeof(magic))
-      .ToString(true);
-}
+const size_t kFooterSize = sizeof(StreamID) + sizeof(MessageSequenceNumber);
 
 }  // namespace
 
-std::string WrapMessage(std::string raw_message,
+std::string WrapMessage(std::string message,
                         StreamID origin,
                         MessageSequenceNumber seqno) {
-  std::string wrapped_message;
-  wrapped_message.reserve(raw_message.size() + kHeaderSize + kFooterSize);
-  // Prefix message with header magic bytes.
-  PutFixed32(&wrapped_message, kHeaderMagic);
-  // Put the RocketSpeed message.
-  wrapped_message.append(raw_message);
+  message.reserve(message.size() + kFooterSize);
   // Append origin stream.
-  EncodeOrigin(&wrapped_message, origin);
+  EncodeOrigin(&message, origin);
   // Append message sequence number
-  PutFixed32(&wrapped_message, seqno);
-  // Suffix message with magic bytes.
-  PutFixed32(&wrapped_message, kFooterMagic);
-  return wrapped_message;
+  PutFixed32(&message, seqno);
+  return message;
 }
 
 Status UnwrapMessage(std::string wrapped_message,
                      std::string* raw_message,
                      StreamID* stream,
                      MessageSequenceNumber* seqno) {
-  if (wrapped_message.size() < kHeaderSize + kFooterSize) {
-    return Status::IOError("Message too short to contain header and footer.");
+  if (wrapped_message.size() < kFooterSize) {
+    return Status::IOError("Message too short to contain footer.");
   }
 
-  // We'll accumulate description of all encountered errors and try to
-  // opportunistically deserialise as much of a message as possible.
-  std::string errors;
-
-  {  // Read the header.
-    Slice in(wrapped_message.data(), kHeaderSize);
-    uint32_t header_magic;
-    if (!GetFixed32(&in, &header_magic)) {
-      errors += "failed reding header magic, ";
-    } else if (header_magic != kHeaderMagic) {
-      errors += MagicToHexString(kHeaderMagic) + " != " +
-                MagicToHexString(header_magic) + " in header, ";
-    }
+  // Read the footer.
+  Slice in(wrapped_message.data() + (wrapped_message.size() - kFooterSize),
+           kFooterSize);
+  // Deserialize origin stream.
+  if (!DecodeOrigin(&in, stream)) {
+    return Status::IOError("Failed deserializing origin stream.");
   }
-
-  {  // Read the footer.
-    Slice in(wrapped_message.data() + (wrapped_message.size() - kFooterSize),
-             kFooterSize);
-    // Deserialize origin stream.
-    if (!DecodeOrigin(&in, stream)) {
-      errors += "failed reading origin, ";
-    }
-    // Deserialize message sequence number.
-    uint32_t seqno_unsigned;
-    if (!GetFixed32(&in, &seqno_unsigned)) {
-      errors += "failed reading seqno, ";
-    } else {
-      *seqno = static_cast<MessageSequenceNumber>(seqno_unsigned);
-    }
-    // Deserialize footer magic.
-    uint32_t footer_magic;
-    if (!GetFixed32(&in, &footer_magic)) {
-      errors += "failed reading footer magic, ";
-    } else if (footer_magic != kFooterMagic) {
-      errors += MagicToHexString(kFooterMagic) + " != " +
-                MagicToHexString(footer_magic) + " in footer, ";
-    }
+  // Deserialize message sequence number.
+  uint32_t seqno_unsigned;
+  if (!GetFixed32(&in, &seqno_unsigned)) {
+    return Status::IOError("Failed deserializing message sequence number.");
   }
+  *seqno = static_cast<MessageSequenceNumber>(seqno_unsigned);
 
   // Strip header and footer and leave a serialized message only.
-  wrapped_message.erase(0, kHeaderSize);
   wrapped_message.resize(wrapped_message.size() - kFooterSize);
   *raw_message = std::move(wrapped_message);
-  return errors.empty() ? Status::OK() : Status::IOError(std::move(errors));
+  return Status::OK();
 }
 
 Status UnwrapMessage(std::string wrapped_message,
