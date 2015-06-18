0d8c4ff7c033909eca69333b54a59a97ac920a8a
pja <pja@fb.com>
2015-06-18 08:13:28 -0700

    Ordered processor lossy, SkipNext, and uint64_t seqno
    Summary:
    Implements the lossy ordered processor as described in task and changes seqno to uint64_t.
    
    Haven't implemented SkipSequenceNumber since it isn't really needed, and is non-trivial.
    
    Test Plan: make check all
    
    Reviewers: #rocketspeed, eduardosuarez
    
    Reviewed By: #rocketspeed, eduardosuarez
    
    Subscribers: dhruba
    
    Differential Revision: https://reviews.facebook.net/D40365

--- src/util/common/ordered_processor.h
+++ src/util/common/ordered_processor.h
@@ -7,10 +7,24 @@
 
 #include <assert.h>
 #include <functional>
+#include <limits>
 #include <memory>
 #include "include/Status.h"
 
 namespace rocketspeed {
+
+enum class OrderedProcessorMode {
+  /**
+   * In lossless mode, buffer overflows are unrecoverable.
+   */
+  kLossless = 1,
+
+  /**
+   * In lossy mode, buffer overflows cause the processor to drop the next
+   * expected message to ensure that the processor always advances.
+   */
+  kLossy = 2,
+};
 
 /**
  * Utility for processing a numbered, but out of order sequence of events in
@@ -33,48 +47,51 @@ struct OrderedProcessor {
 
   struct Unprocessed {
     Unprocessed()
-    : seqno(-1) {}
+    : seqno(kInvalidSeqno) {}
 
     Unprocessed(T _object, int _seqno)
     : object(std::move(_object))
     , seqno(_seqno) {}
 
     T object;
-    int seqno;
+    uint64_t seqno;
   };
 
-  OrderedProcessor(int buffer_size,
-                   std::function<void(T)> processor)
+  OrderedProcessor(
+      int buffer_size,
+      std::function<void(T)> processor,
+      OrderedProcessorMode mode = OrderedProcessorMode::kLossless)
   : buffer_(new Unprocessed[buffer_size])
   , buffer_size_(buffer_size)
   , next_seqno_(0)
-  , processor_(std::move(processor)) {
+  , processor_(std::move(processor))
+  , mode_(mode) {
     assert(processor_);
   }
 
-  Status Process(T object, int seqno) {
+  Status Process(T object, uint64_t seqno) {
     if (seqno == next_seqno_) {
-      // This was the next expected seqno, so process immediately.
+      // This was the next expected seqno, so process immediately, and then
+      // process any additional messages at the head.
       processor_(std::move(object));
       ++next_seqno_;
-
-      // Check if there were any enqueued objects to process and process them.
-      int head = next_seqno_ % buffer_size_;
-      while (buffer_[head].seqno == next_seqno_) {
-        processor_(std::move(buffer_[head].object));
-        buffer_[head].seqno = -1;
-        ++next_seqno_;
-        if (++head == buffer_size_) {
-          head = 0;
-        }
-      }
+      ProcessHead();
       return Status::OK();
     }
 
     // Object has arrived out of order.
     // First, check if it fits in the buffer.
-    if (seqno > next_seqno_ + buffer_size_) {
-      return Status::NoBuffer();
+    Status st;
+    if (mode_ == OrderedProcessorMode::kLossy) {
+      // Skip head records until this record fits in the buffer.
+      while (seqno > next_seqno_ + buffer_size_) {
+        SkipNext();
+      }
+      st = Status::NoBuffer();
+    } else if (mode_ == OrderedProcessorMode::kLossless) {
+      if (seqno > next_seqno_ + buffer_size_) {
+        return Status::NoBuffer();
+      }
     }
 
     if (seqno < next_seqno_) {
@@ -82,14 +99,14 @@ struct OrderedProcessor {
     }
 
     // Fits in buffer, check that it isn't a duplicate.
-    int index = seqno % buffer_size_;  // location for new oj
-    if (buffer_[index].seqno != -1) {
+    uint64_t index = seqno % buffer_size_;  // location for new obj
+    if (buffer_[index].seqno != kInvalidSeqno) {
       return Status::InvalidArgument("Duplicate sequence number provided");
     }
 
     buffer_[index].seqno = seqno;
     buffer_[index].object = std::move(object);
-    return Status::OK();
+    return st;
   }
 
   /**
@@ -98,16 +115,45 @@ struct OrderedProcessor {
    */
   void Reset() {
     next_seqno_ = 0;
-    for (int i = 0; i < buffer_size_; ++i) {
+    for (uint64_t i = 0; i < buffer_size_; ++i) {
       buffer_[i] = Unprocessed();
+    }
+  }
+
+  /**
+   * Skips the next expected sequence number.
+   */
+  void SkipNext() {
+    ++next_seqno_;
+    ProcessHead();
+  }
+
+ private:
+  enum : uint64_t {
+    kInvalidSeqno = std::numeric_limits<uint64_t>::max()
+  };
+
+  /**
+   * Process any pending objects at the head.
+   */
+  void ProcessHead() {
+    uint64_t head = next_seqno_ % buffer_size_;
+    while (buffer_[head].seqno == next_seqno_) {
+      processor_(std::move(buffer_[head].object));
+      buffer_[head].seqno = kInvalidSeqno;
+      ++next_seqno_;
+      if (++head == buffer_size_) {
+        head = 0;
+      }
     }
   }
 
  private:
   std::unique_ptr<Unprocessed[]> buffer_;  // cyclic buffer of unprocessed data
-  int buffer_size_;                        // size of buffer_
-  int next_seqno_;                         // seqno of next object to process
+  uint64_t buffer_size_;                   // size of buffer_
+  uint64_t next_seqno_;                    // seqno of next object to process
   std::function<void(T)> processor_;       // function for processing data
+  OrderedProcessorMode mode_;
 };
 
 }  // namespace rocketspeed
--- src/util/tests/ordered_processor_test.cc
+++ src/util/tests/ordered_processor_test.cc
@@ -10,6 +10,8 @@
 #include "src/util/common/ordered_processor.h"
 #include <algorithm>
 #include <array>
+#include <set>
+#include <vector>
 
 namespace rocketspeed {
 
@@ -22,39 +24,39 @@ TEST(OrderedProcessorTest, Basic) {
 
   // seqno 0, should be process immediately
   ASSERT_OK(p.Process(100, 0));
-  ASSERT_TRUE(processed == (Ints{100}));
+  ASSERT_EQ(processed, (Ints{100}));
 
   // seqno 2, should not be processed yet.
   ASSERT_OK(p.Process(300, 2));
-  ASSERT_TRUE(processed == (Ints{100}));
+  ASSERT_EQ(processed, (Ints{100}));
 
   // seqno 1, should be processed, then seqno 2 which was already queued.
   ASSERT_OK(p.Process(200, 1));
-  ASSERT_TRUE(processed == (Ints{100, 200, 300}));
+  ASSERT_EQ(processed, (Ints{100, 200, 300}));
 
-  // seqno 0, 1 and 2, 6 should fail to process
+  // seqno 0, 1 and 2, 7 should fail to process
   ASSERT_TRUE(p.Process(666, 0).IsInvalidArgument());  // in past
   ASSERT_TRUE(p.Process(666, 1).IsInvalidArgument());  // in past
   ASSERT_TRUE(p.Process(666, 2).IsInvalidArgument());  // in past
   ASSERT_TRUE(p.Process(666, 7).IsNoBuffer());  // not enough buffer space
-  ASSERT_TRUE(processed == (Ints{100, 200, 300}));
+  ASSERT_EQ(processed, (Ints{100, 200, 300}));
 
   // seqno 4, 5, 6 should not be processed yet.
   ASSERT_OK(p.Process(500, 4));
   ASSERT_OK(p.Process(700, 6));
   ASSERT_OK(p.Process(600, 5));
-  ASSERT_TRUE(processed == (Ints{100, 200, 300}));
+  ASSERT_EQ(processed, (Ints{100, 200, 300}));
 
   // seqno 3, should process all.
   ASSERT_OK(p.Process(400, 3));
-  ASSERT_TRUE(processed == (Ints{100, 200, 300, 400, 500, 600, 700}));
+  ASSERT_EQ(processed, (Ints{100, 200, 300, 400, 500, 600, 700}));
 
   // reset processor without changing processor functor
   p.Reset();
 
   // seqno 0, should process immediately
   ASSERT_OK(p.Process(800, 0));
-  ASSERT_TRUE(processed == (Ints{100, 200, 300, 400, 500, 600, 700, 800}));
+  ASSERT_EQ(processed, (Ints{100, 200, 300, 400, 500, 600, 700, 800}));
 }
 
 TEST(OrderedProcessorTest, Randomized) {
@@ -76,6 +78,83 @@ TEST(OrderedProcessorTest, Randomized) {
   for (int i = 0; i < n; ++i) {
     ASSERT_EQ(processed[i], i);
   }
+}
+
+TEST(OrderedProcessorTest, SkipNext) {
+  typedef std::vector<int> Ints;
+  Ints processed;
+  OrderedProcessor<int> p(3, [&] (int x) { processed.push_back(x); });
+
+  // seqno 0, should be process immediately
+  ASSERT_OK(p.Process(100, 0));
+  ASSERT_EQ(processed, (Ints{100}));
+
+  // seqno 2, should not be processed yet.
+  ASSERT_OK(p.Process(300, 2));
+  ASSERT_EQ(processed, (Ints{100}));
+
+  // skip seqno 1, then seqno 2 which was already queued.
+  p.SkipNext();
+  ASSERT_EQ(processed, (Ints{100, 300}));
+
+  // seqno 0, 1 and 2, 7 should fail to process
+  ASSERT_TRUE(p.Process(666, 0).IsInvalidArgument());  // in past
+  ASSERT_TRUE(p.Process(666, 1).IsInvalidArgument());  // in past
+  ASSERT_TRUE(p.Process(666, 2).IsInvalidArgument());  // in past
+  ASSERT_TRUE(p.Process(666, 7).IsNoBuffer());  // not enough buffer space
+  ASSERT_EQ(processed, (Ints{100, 300}));
+
+  // seqno 4, 5, 6 should not be processed yet.
+  ASSERT_OK(p.Process(500, 4));
+  ASSERT_OK(p.Process(700, 6));
+  ASSERT_OK(p.Process(600, 5));
+  ASSERT_EQ(processed, (Ints{100, 300}));
+
+  // skip 3, should process all.
+  p.SkipNext();
+  ASSERT_EQ(processed, (Ints{100, 300, 500, 600, 700}));
+
+  // reset processor without changing processor functor
+  p.Reset();
+
+  // skip 3 then process 3, should process immediately
+  p.SkipNext();
+  p.SkipNext();
+  p.SkipNext();
+  ASSERT_OK(p.Process(800, 3));
+  ASSERT_EQ(processed, (Ints{100, 300, 500, 600, 700, 800}));
+}
+
+TEST(OrderedProcessorTest, LossyRandomized) {
+  const int n = 100000;
+  const int buffer = 1000;
+  std::vector<int> processed;
+  OrderedProcessor<int> p(buffer, [&] (int x) { processed.push_back(x); },
+    OrderedProcessorMode::kLossy);
+
+  // Generate numbers 0..n in random order.
+  std::vector<int> numbers(n);
+  std::iota(numbers.begin(), numbers.end(), 0);
+  std::random_device rd;
+  std::mt19937 rng(rd());
+  std::shuffle(numbers.begin(), numbers.end(), rng);
+
+  // Process first 100.
+  std::set<int> expected_set;
+  for (int x : numbers) {
+    p.Process(x, x);
+    if (expected_set.empty() || x >= *expected_set.rbegin() - buffer) {
+      expected_set.insert(x);
+    }
+  }
+
+  // Skip 100 to ensure full buffer is processed.
+  for (size_t i = 0; i < buffer; ++i) {
+    p.SkipNext();
+  }
+
+  std::vector<int> expected(expected_set.begin(), expected_set.end());
+  ASSERT_EQ(processed, expected);
 }
 
 }  // namespace rocketspeed
