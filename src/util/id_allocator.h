c0bb539ad703621b526e108c2d470388d8642faf
Mateusz Machalica <stupaq@fb.com>
2015-07-16 02:30:30 -0700

    Better ID allocation scheme.
    Summary: To make allocators faster and debugging much easier.
    
    Test Plan: `make -j all check`
    
    Reviewers: #rocketspeed, pja
    
    Reviewed By: #rocketspeed, pja
    
    Differential Revision: https://reviews.facebook.net/D42111

--- src/util/id_allocator.h
+++ src/util/id_allocator.h
@@ -1,4 +1,4 @@
-//  Copyright (c) 2014, Facebook, Inc.  All rights reserved.
+//  Copyright (c) 2015, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under the BSD-style license found in the
 //  LICENSE file in the root directory of this source tree. An additional grant
 //  of patent rights can be found in the PATENTS file in the same directory.
@@ -16,9 +16,10 @@ namespace rocketspeed {
 /**
  * An allocator for arbitrary type of numeric IDs.
  *
- * Each allocator has a set of allowed IDs and will only return IDs from it.
- * Current implementation always represents a range of IDs, allocates them one
- * by one and wraps around if runs out of unique IDs.
+ * Each allocator identifies a set of allowed IDs and will only return IDs from
+ * this set. Since the set is finite, every allocator will eventually return the
+ * same ID twice. The allocator specialised for any numeric type has a period
+ * equal to the size of the set of allowed IDs.
  */
 template <typename IDType, typename AllocatorType>
 class IDAllocator {
@@ -29,12 +30,10 @@ class IDAllocator {
   // Movable
   IDAllocator(IDAllocator&& other) noexcept { *this = std::move(other); }
   IDAllocator& operator=(IDAllocator&& other) {
-    first_ = other.first_;
-    end_ = other.end_;
+    step_ = other.step_;
     next_ = other.next_;
     // Invalidate other.
-    other.first_ = kGlobalEnd;
-    other.end_ = kGlobalFirst;
+    other.step_ = 0;
     return *this;
   }
 
@@ -43,58 +42,40 @@ class IDAllocator {
    * Do not use this one if you want to reserve some special values that should
    * never be allocated.
    */
-  IDAllocator() : IDAllocator(kGlobalFirst, kGlobalEnd) { CheckValid(); }
+  IDAllocator() : step_(1), next_(0) { CheckValid(); }
 
-  /** Creates an allocator for given range of IDs. */
-  IDAllocator(IDType first, IDType end)
-      : first_(first), end_(end), next_(first_) {
-    CheckValid();
-  }
+  IDAllocator(IDType step, IDType next) : step_(step), next_(next) {}
 
+  /**
+   * Returns next allowed ID, advances allocator.
+   */
   IDType Next() {
     CheckValid();
-    IDType res = next_;
-    if (IsLast()) {
-      next_ = first_;
-    } else {
-      ++next_;
-    }
-    return res;
-  }
-
-  bool IsLast() const {
-    CheckValid();
-    return next_ + 1 == end_;
-  }
-
-  bool IsEmpty() const {
-    CheckValid();
-    return first_ == end_;
+    IDType curr = next_;
+    next_ = static_cast<IDType>(next_ + step_);
+    return curr;
   }
 
   bool IsSourceOf(IDType id) const {
     CheckValid();
-    return first_ <= id && id < end_;
-  }
-
-  size_t TotalSize() const {
-    CheckValid();
-    return end_ - first_;
+    IDType step_mask = step_;
+    --step_mask;
+    return (next_ & step_mask) == (id & step_mask);
   }
 
   /**
    * Breaks allocator's set of remaining available IDs in two (as equal as
-   * possible) subsets. This retains the lower part of the set.
+   * possible) subsets. This retains one of the parts, the other one is
+   * returned.
    *
-   * @return An allocator representing the upper part.
+   * @return An allocator representing one of the parts.
    */
   AllocatorType Split() {
     CheckValid();
-    IDType mid = next_ + (end_ - next_) / 2;
-    AllocatorType upper(mid, end_);
-    upper.Next();
-    end_ = mid;
-    return upper;
+    auto allocs = Divide(2);
+    assert(allocs.size() == 2);
+    *this = std::move(allocs.front());
+    return std::move(allocs.back());
   }
 
   /**
@@ -108,29 +89,22 @@ class IDAllocator {
    */
   class DivisionMapping {
    public:
-    DivisionMapping() = default;
+    DivisionMapping() : shift_(-1), mask_(0) {}
 
     size_t operator()(IDType id) const {
-      assert(id >= first_);
-      size_t index = (id - first_) / min_size_;
-      if (index >= num_pieces_) {
-        index = num_pieces_ - 1;
-      }
-      return index;
+      assert(shift_ >= 0);
+      return (id >> shift_) & mask_;
     }
 
    private:
     friend class IDAllocator<IDType, AllocatorType>;
 
-    /** First allowed ID of the whole range. */
-    IDType first_;
-    /** Total number of pieces resulting from the division. */
-    size_t num_pieces_;
-    /** Size of the smallest piece. */
-    IDType min_size_;
+    /** Step of an allocator before splitting. */
+    int shift_;
+    /** Multiplicative difference between steps before and after splitting. */
+    IDType mask_;
 
-    DivisionMapping(IDType first, size_t num_pieces, IDType min_size)
-        : first_(first), num_pieces_(num_pieces), min_size_(min_size) {}
+    DivisionMapping(int shift, IDType mask) : shift_(shift), mask_(mask) {}
   };
 
   /**
@@ -139,52 +113,64 @@ class IDAllocator {
    * available IDs contains the union of sets of available IDs of
    * returned allocators. The converse is not necessarily true.
    *
+   * @param min_pieces Minimal number of pieces to split allocator into.
    * @param mapping A mapping, from ID to index in returned list, see above.
    *                If null, no mapping will be returned.
    * @return A list of disjoint allocators, their union is included in this.
    */
-  std::vector<AllocatorType> Divide(size_t num_pieces,
+  std::vector<AllocatorType> Divide(size_t min_pieces,
                                     DivisionMapping* mapping = nullptr) const {
     CheckValid();
-    assert(num_pieces < TotalSize());
-    // Whatever implementation we go with, we need the reverse mapping from
-    // ID to the index in returned vector (given the original, divided
-    // allocator) to be quickly computable
+    // Find the actual number of pieces, which is always a power of two.
+    IDType num_pieces = 1;
+    IDType new_step = step_;
+    while (num_pieces < min_pieces) {
+      assert(new_step <= (kMax << 1));
+      num_pieces = static_cast<IDType>(num_pieces << 1);
+      new_step = static_cast<IDType>(new_step << 1);
+    }
+
     std::vector<AllocatorType> allocs;
     allocs.reserve(num_pieces);
-    const IDType min_size = static_cast<IDType>((end_ - first_) / num_pieces);
-    IDType first = first_;
-    IDType end;
-    for (size_t piece = 0; piece < num_pieces; ++piece) {
-      end = piece == num_pieces - 1 ? end_
-                                    : static_cast<IDType>(first + min_size);
-      assert(first_ <= first);
-      assert(first <= end);
-      assert(end <= end_);
-      allocs.push_back(AllocatorType(first, end));
-      first = end;
+
+    // Normalise next value, as if the allocator was never used.
+    IDType first = next_ & static_cast<IDType>(step_ - 1);
+
+    AllocatorType piece(step_, first);
+    for (size_t i = 0; i < num_pieces; ++i) {
+      allocs.emplace_back(new_step, piece.next_);
+      piece.Next();
     }
+
     // Set the mapping appropriately.
     if (mapping) {
-      *mapping = DivisionMapping(first_, num_pieces, min_size);
+      int shift = -1;
+      IDType step = step_;
+      while (step > 0) {
+        ++shift;
+        step = static_cast<IDType>(step >> 1);
+      }
+      *mapping = DivisionMapping(shift, num_pieces - 1);
     }
     return allocs;
   }
 
  private:
-  static constexpr IDType kGlobalFirst = std::numeric_limits<IDType>::min();
-  static constexpr IDType kGlobalEnd = std::numeric_limits<IDType>::max();
+  static_assert(std::numeric_limits<IDType>::is_integer,
+                "ID type must be integer type");
+  static_assert(!std::numeric_limits<IDType>::is_signed,
+                "ID type must be unsigned type");
 
-  /** First ID available for this allocator. */
-  IDType first_;
-  /** The first ID not available for this allocator after first_. */
-  IDType end_;
+  static constexpr IDType kMax = std::numeric_limits<IDType>::max();
+
+  /** Step of this ID allocator. */
+  IDType step_;
   /** Next ID that can be allocated. */
   IDType next_;
 
   void CheckValid() const {
-    assert(first_ <= next_);
-    assert(next_ <= end_);
+    assert(step_ > 0);
+    assert((step_ & (step_ - 1)) == 0);
   }
 };
 
