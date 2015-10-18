ceb89e10f7ca1069f2df6a56b05ac1fa3ebe7974
Dhruba Borthakur <dhruba@fb.com>
2015-10-18 16:30:29 -0700

    Do not print out progress-status very frequently in Test3.
    Summary:
    Do not print out progress-status very frequently in Test3.
    
    Test Plan:
    run
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- src/tools/rocketbench/Test3.sh
+++ src/tools/rocketbench/Test3.sh
@@ -13,11 +13,11 @@ if [ ! -f $BENCHMARK ]; then
 fi
 
 # Produce a data set with 1 billion messages spread across 10K topics
-cmd="$BENCHMARK --messages 250000000 --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --cockpits=4 --towers=1 --remote-bench 4 produce"
+cmd="$BENCHMARK --messages 250000000 --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --buffered_storage_max_latency_us=0 --cockpits=4 --towers=1 --remote-bench 4 produce"
 echo $cmd
 eval $cmd
 
 # Subscribe with a backlog, do not produce any new data
-cmd="$BENCHMARK --messages 250000000 --subscribe-rate 100000 --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --cache-size 1000000000 --socket-buffer-size=157286400 --cockpits=4 --towers=1 --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
+cmd="$BENCHMARK --messages 250000000 --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --cache-size 1000000000 --socket-buffer-size=157286400 --cockpits=4 --towers=1 --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
 echo $cmd
 eval $cmd
