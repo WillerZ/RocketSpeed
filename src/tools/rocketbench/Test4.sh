e400248ac0a908f7c11f0e666df476602f5e0abe
Dhruba Borthakur <dhruba@fb.com>
2015-10-18 16:46:57 -0700

    Print progress-status for Test4 once every 10 seconds
    Summary:
    Print progress-status for Test4 once every 10 seconds
    
    Test Plan:
    run
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- src/tools/rocketbench/Test4.sh
+++ src/tools/rocketbench/Test4.sh
@@ -15,6 +15,6 @@ fi
 # Subscribe to a single topic from 80K clients
 # then publish 1 message to that topic and wait to read all messages
 # from all the clients.
-cmd="$BENCHMARK --messages 1 --subscribe-rate 1000000 --rate 150000 --max-inflight 7000 --topics 1 --size 100 --remote --deploy --start-servers --stop-servers --subscription-backlog-distribution=fixed --client-threads=10000 --remote-bench 8 --max_file_descriptors=1000000 --namespaceid_dynamic --towers=1 --cockpits=1 readwrite"
+cmd="$BENCHMARK --messages 1 --subscribe-rate 1000000 --rate 150000 --max-inflight 7000 --topics 1 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --subscription-backlog-distribution=fixed --client-threads=10000 --remote-bench 8 --max_file_descriptors=1000000 --namespaceid_dynamic --towers=1 --cockpits=1 readwrite"
 echo $cmd
 eval $cmd
