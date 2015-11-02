1223b8b93cf353d65ce0624067c392b2bdd9510c
Dhruba Borthakur <dhruba@fb.com>
2015-11-02 15:22:42 -0800

    Use 3 control towers and 2 copilots for Test3.
    Summary:
    The entire data-set is still not cached in the control-tower
    but this improves the performance 5x.
    
    My goal is to keep iterating on the 100 GB data-set to fit in the memory of 3 control towers.
    
    Test Plan: run benchmark
    
    Reviewers: pja, eduardosuarez, stupaq-fb
    
    Reviewed By: stupaq-fb
    
    Subscribers: rocketspeed, stupaq-fb, dhruba
    
    Differential Revision: https://reviews.facebook.net/D50049

--- src/tools/rocketbench/Test3.sh
+++ src/tools/rocketbench/Test3.sh
@@ -6,6 +6,10 @@
 base=`dirname $0`
 BENCHMARK=$base/benchmark.sh
 
+# Define the number of cockpits and control towers for this test
+COCKPITS=2
+TOWERS=3
+
 if [ ! -f $BENCHMARK ]; then
   echo "Unable to find benchmark script at $BENCHMARK
   echo "cd to fbcode and then run benchmark
@@ -15,7 +19,7 @@ fi
 cleanup() {
   # Stop servers
   echo "Stopping servers......"
-  cmd="$BENCHMARK --remote --stop-servers --cockpits=4 --towers=1 "
+  cmd="$BENCHMARK --remote --stop-servers --cockpits=$COCKPITS --towers=$TOWERS "
   echo $cmd
   eval $cmd
 }
@@ -23,24 +27,24 @@ cleanup() {
 trap cleanup 0
 
 # Produce a data set with 1 billion messages spread across 10K topics
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --buffered_storage_max_latency_us=0 --cockpits=4 --towers=1 --namespaceid_dynamic --remote-bench 4 produce"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --buffered_storage_max_latency_us=0 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --remote-bench 4 produce"
 echo $cmd
 eval $cmd || { echo "Produce stage failed"; exit 1; }
 
-# Start servers
+# Start servers with a 50 GB cache on each control tower.
 echo "Starting server......"
-cmd="$BENCHMARK --remote --deploy --start-servers --cache-size 1000000000 --socket-buffer-size=157286400 --cockpits=4 --towers=1 "
+cmd="$BENCHMARK --remote --deploy --start-servers --cache-size 50000000000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS "
 echo $cmd
 eval $cmd || { echo "Failed to start servers"; exit 1; }
 
-# Subscribe with a backlog, do not produce any new data. Fill up the 1GB cache.
+# Subscribe with a backlog, do not produce any new data. Fill up the cache.
 echo "Filling cache...."
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=4 --towers=1 --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
 echo $cmd
 eval $cmd || { echo "Failed to fill up cache"; exit 1; }
 
 # Subscribe again with a backlog, Use data from cache.
 echo "Measurement started...."
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=4 --towers=1 --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages 250000000 --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
 echo $cmd
 eval $cmd || { echo "Failed to read backlog"; exit 1; }
