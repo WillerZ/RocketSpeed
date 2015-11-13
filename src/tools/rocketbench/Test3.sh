a200b1da91f3ca46ba9bd92cc3f6b0b1249156c9
Dhruba Borthakur <dhruba@fb.com>
2015-11-13 03:25:55 -0800

    Make Test3 use 30K topics.
    Summary:
    Make Test3 use 30K topics.
    
    Test Plan:
    run benchmark
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- src/tools/rocketbench/Test3.sh
+++ src/tools/rocketbench/Test3.sh
@@ -10,6 +10,8 @@ BENCHMARK=$base/benchmark.sh
 COCKPITS=2
 TOWERS=3
 NUMMSG=250000000
+TOPICS_PER_BENCH=7500
+BENCH=4
 
 if [ ! -f $BENCHMARK ]; then
   echo "Unable to find benchmark script at $BENCHMARK
@@ -28,7 +30,7 @@ cleanup() {
 trap cleanup 0
 
 # Produce a data set with 1 billion messages spread across 10K topics
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --rate 150000 --max-inflight 7000 --topics 10000 --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --buffered_storage_max_latency_us=0 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --remote-bench 4 produce"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --rate 150000 --max-inflight 7000 --topics $TOPICS_PER_BENCH --size 100 --remote --deploy --start-servers --stop-servers --progress_period=10000 --buffered_storage_max_latency_us=0 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --remote-bench $BENCH produce"
 echo $cmd
 eval $cmd || { echo "Produce stage failed"; exit 1; }
 
@@ -40,12 +42,18 @@ eval $cmd || { echo "Failed to start servers"; exit 1; }
 
 # Subscribe with a backlog, do not produce any new data. Fill up the cache.
 echo "Filling cache...."
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --subscribe-rate 10000 --max-inflight 7000 --topics $TOPICS_PER_BENCH --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench $BENCH --producer=false consume"
 echo $cmd
 eval $cmd || { echo "Failed to fill up cache"; exit 1; }
 
 # Subscribe again with a backlog, Use data from cache.
 echo "Measurement started...."
-cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --subscribe-rate 10000 --max-inflight 7000 --topics 10000 --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench 4 --producer=false consume"
+cmd="$BENCHMARK --collect-stats --progress_per_line --messages $NUMMSG --subscribe-rate 10000 --max-inflight 7000 --topics $TOPICS_PER_BENCH --size 100 --remote --progress_period=10000 --socket-buffer-size=157286400 --cockpits=$COCKPITS --towers=$TOWERS --namespaceid_dynamic --subscription-backlog-distribution=uniform --remote-bench $BENCH --producer=false consume"
 echo $cmd
 eval $cmd || { echo "Failed to read backlog"; exit 1; }
+
+# Stop servers explicitly (even though the bash-trap stops them too).
+echo Stopping Servers...
+cmd="$BENCHMARK --remote --stop-servers --cockpits=$COCKPITS --towers=$TOWERS "
+eval $cmd
+
