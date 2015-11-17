37c4fb50144c579824c607cfa01a98bacb3f3af8
Dhruba Borthakur <dhruba@fb.com>
2015-11-17 14:27:36 -0800

    Refactor Test1 to make it compatible for easy modification of test size.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- src/tools/rocketbench/Test1.sh
+++ src/tools/rocketbench/Test1.sh
@@ -11,20 +11,36 @@
 base=`dirname $0`
 BENCHMARK=$base/benchmark.sh
 
+# Define the number of cockpits and control towers for this test
+COCKPITS=4
+TOWERS=1
+NUMMSG=250000000
+BENCH=4
+
 if [ ! -f $BENCHMARK ]; then
   echo "Unable to find benchmark script at $BENCHMARK
   echo "cd to fbcode and then run benchmark
   exit 1
 fi
 
+cleanup() {
+  # Stop servers
+  echo "Stopping servers......"
+  cmd="$BENCHMARK --remote --stop-servers --cockpits=$COCKPITS --towers=$TOWERS "
+  echo $cmd
+  eval $cmd
+}
+
+trap cleanup 0
+
 echo Starting Servers...
-cmd="$BENCHMARK --remote --deploy --start-servers --buffered_storage_max_latency_us=1000 --cockpits=4"
+cmd="$BENCHMARK --remote --deploy --start-servers --buffered_storage_max_latency_us=1000 --cockpits=$COCKPITS --towers=$TOWERS"
 eval $cmd
 
-cmd="$BENCHMARK --messages 250000000 --rate 500000 --max-inflight 7000 --topics 1 --size 100 --progress_period=10000 --cockpits=4 --remote-bench 4 produce"
+cmd="$BENCHMARK --messages $NUMMSG --rate 500000 --max-inflight 7000 --topics 1 --size 100 --progress_period=10000 --cockpits=$COCKPITS --towers=$TOWERS --remote-bench $BENCH produce"
 echo $cmd
-eval $cmd
+eval $cmd || { echo "Produce stage failed"; exit 1; }
 
 echo Stopping Servers...
-cmd="$BENCHMARK --remote --stop-servers --cockpits=4"
+cmd="$BENCHMARK --remote --stop-servers --cockpits=$COCKPITS --towers=$TOWERS "
 eval $cmd
