1999a324596dff6cc8e6b8475e515a8e009381de
pja <pja@fb.com>
2015-11-30 07:37:59 -0800

    More subscribers for Test5
    Summary:
    Changes to Test5
    
    * Use 4 rocketbench hosts (instead of 1), each reading 1/4 of the topics.
    * Increase subscribe rate.
    * Start servers, run bench once to fill cache, then run again with full cache.
    
    Test Plan: run it
    
    Reviewers: #rocketspeed, dhruba
    
    Reviewed By: #rocketspeed, dhruba
    
    Subscribers: stupaq-fb, dhruba
    
    Differential Revision: https://reviews.facebook.net/D51399

--- src/tools/rocketbench/Test5.sh
+++ src/tools/rocketbench/Test5.sh
@@ -14,9 +14,9 @@ BENCHMARK=$base/benchmark.sh
 COCKPITS=${COCKPITS:-2}               # Number of pilot/copilot servers
 TOWERS=${TOWERS:-1}                   # Number of control tower servers
 CACHESIZE=${CACHESIZE:-50000000000}   # Control tower cache size (50 GB)
-NUMTOPICS=${NUMTOPICS:-10000000}      # Number of topics to publish to
-BENCH=${BENCH:-1}                     # Number of rocketbench instances
-RATE=${RATE:-500000}                  # max of 500K subscribe requests/sec
+NUMTOPICS=${NUMTOPICS:-2500000}       # Number of topics to publish to
+BENCH=${BENCH:-4}                     # Number of rocketbench instances
+RATE=${RATE:-300000}                  # max of 300K subscribe requests/sec/bench
 
 if [ ! -f $BENCHMARK ]; then
   echo "Unable to find benchmark script at $BENCHMARK"
@@ -37,13 +37,31 @@ trap cleanup 0
 #
 # Produce a data set with 1 message each on a 10 M topics
 #
-cmd="$BENCHMARK --messages $NUMTOPICS --progress_period=10000 --rate 150000 --max-inflight 7000 --topics $NUMTOPICS --size 100 --num_messages_per_topic=$NUMTOPICS  --topics_distribution=fixed --namespaceid_dynamic --remote --deploy --start-servers --stop-servers --cockpits=$COCKPITS --towers=$TOWERS --remote-bench $BENCH produce"
+cmd="$BENCHMARK --messages $NUMTOPICS --progress_period=1000 --progress_per_line --rate 150000 --max-inflight 2000 --topics $NUMTOPICS --size 100 --num_messages_per_topic=$NUMTOPICS  --topics_distribution=fixed --namespaceid_dynamic --remote --deploy --start-servers --stop-servers --cockpits=$COCKPITS --towers=$TOWERS --remote-bench $BENCH produce"
 echo $cmd
 eval $cmd || { echo "Failed to write all messages"; exit 1; }
 
+# Start servers with a 50 GB cache on each control tower.
+echo "Starting server..."
+cmd="$BENCHMARK --remote --deploy --start-servers --cache-size $CACHESIZE --cockpits=$COCKPITS --towers=$TOWERS"
+echo $cmd
+eval $cmd || { echo "Failed to start servers"; exit 1; }
+
 # Subscribe to each of those topics and read one message from each of those
-# topics.
-#
-cmd="$BENCHMARK --messages $NUMTOPICS --progress_period=10000 --subscribe-rate $RATE --max-inflight 20000 --namespaceid_dynamic --topics $NUMTOPICS --remote --deploy --start-servers --stop-servers --cockpits=$COCKPITS --cache-size $CACHESIZE --towers=$TOWERS --subscription-backlog-distribution=fixed --remote-bench $BENCH --client-threads=80 --producer=false --collect-stats consume"
+# topics to fill the cache.
+echo "Filling cache..."
+cmd="$BENCHMARK --messages $NUMTOPICS --progress_period=1000 --progress_per_line --subscribe-rate $((RATE/2)) --max-inflight 20000 --namespaceid_dynamic --topics $NUMTOPICS --remote --cockpits=$COCKPITS --cache-size $CACHESIZE --towers=$TOWERS --subscription-backlog-distribution=fixed --remote-bench $BENCH --client-threads=80 --producer=false --collect-stats consume"
 echo $cmd
 eval $cmd || { echo "Failed to receive all messages"; exit 1; }
+
+# Subscribe to each of those topics and read one message from each of those
+# topics.
+echo "Measuring subscription throughput..."
+cmd="$BENCHMARK --messages $NUMTOPICS --progress_period=1000 --progress_per_line --subscribe-rate $RATE --max-inflight 20000 --namespaceid_dynamic --topics $NUMTOPICS --remote --cockpits=$COCKPITS --cache-size $CACHESIZE --towers=$TOWERS --subscription-backlog-distribution=fixed --remote-bench $BENCH --client-threads=80 --producer=false --collect-stats consume"
+echo $cmd
+eval $cmd || { echo "Failed to receive all messages"; exit 1; }
+
+# Stop servers explicitly (even though the bash-trap stops them too).
+echo Stopping Servers...
+cmd="$BENCHMARK --remote --stop-servers --cockpits=$COCKPITS --towers=$TOWERS "
+eval $cmd
